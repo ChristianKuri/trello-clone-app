1f8258564de06dc32d300442285ca50a539efc09
Christian Kuri <christian.kuri.martinez@gmail.com>
2022-03-07 11:47:56 -0600

    Add Font Awesome

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Icons
+import FontAwesomePlugin from './plugins/fontawesome'
+
 // Styles
 import './assets/main.css'
 
-const app = createApp(App)
 const pinia = createPinia()
 
-app.use(pinia)
-
-app.mount('#app')
+createApp(App)
+  .use(pinia)
+  .use(FontAwesomePlugin)
+  .mount('#app')

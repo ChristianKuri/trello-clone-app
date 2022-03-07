5b67631c316a982352d32540e682689f62e9df37
Christian Kuri <christian.kuri.martinez@gmail.com>
2022-03-07 11:39:06 -0600

    config prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,13 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
+  root: true,
+  extends: [
     "eslint:recommended",
-    "@vue/eslint-config-prettier"
+    "vue/vue3-recommended",
+    "@vue/eslint-config-prettier",
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
-}
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+};

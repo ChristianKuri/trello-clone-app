cdf1b8e0008f6624b15ba958588ebff74e1cd07d
Christian Kuri <christian.kuri.martinez@gmail.com>
2022-03-09 10:21:37 -0600

    Add localstorage to the pinia storage

--- src/stores/BoardStore.js
+++ src/stores/BoardStore.js
@@ -1,10 +1,11 @@
 import { defineStore } from 'pinia'
+import { useLocalStorage } from '@vueuse/core'
 import boardData from '../data/default-board'
 
 export const useBoardStore = defineStore('BoardStore', {
   state: () => {
     return {
-      data: boardData
+      data: useLocalStorage('BoardStore:data', boardData)
     }
   }
 })
